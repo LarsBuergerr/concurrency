93185f4ba98d8eed82b3a5b90321f28d44ef05d2
Lars Buerger <lars.buerger@freenet.de>
2025-03-28 17:54:27 +0100

    Refactor Problem2: implement concurrent processing with ExecutorService, enhance cache validation, and improve factorization logic

--- app/src/main/java/concurrency/exercise2/Problem2.java
+++ app/src/main/java/concurrency/exercise2/Problem2.java
@@ -3,88 +3,120 @@ package concurrency.exercise2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Problem2 {
-  
-
-  public static void main(String[] args) {
-    Cache cache = new Cache();
-    Request request = new Request(20);
-    Service service = new Service(request, cache);
-    service.run();
-
-    System.out.println(cache.toString());
-  }
+    public static void main(String[] args) throws InterruptedException {
+        Cache cache = new Cache();
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        
+        for (int i = 0; i < 1000000000; i++) {
+            int[] randomNums = new int[]{6, 8};
+            int randomIndex = (int) (Math.random() * randomNums.length);
+            Request request = new Request(randomNums[randomIndex]);
+            executor.execute(new Service(request, cache));
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        
+        System.out.println(cache.toString());
+    }
 }
-
-
 
 class Service implements Runnable {
+    private final Request request;
+    private final Cache cache;
 
-  private final Request request;
-  private final Cache cache;
-
-  public Service(Request request, Cache cache) {
-    this.request = request;
-    this.cache = cache;
-  }
-
-  @Override
-  public void run() {
-    Integer[] factors = this.factorsOf(request.content);
-
-    cache.setCache(request.content, factors);
-    Response response = new Response(factors);
-    System.out.println("The factors of " + request.content + " are: " + Arrays.toString(response.content));    
-  }
-
-  public Integer[] factorsOf(int val) {
-    List<Integer> numArray = new ArrayList<Integer>();
-
-    for (int i = 2; i <= Math.ceil(Math.sqrt(val)); i++) {
-        if (val % i == 0) {
-            numArray.add(i);
-            val /= i;
-        }
+    public Service(Request request, Cache cache) {
+        this.request = request;
+        this.cache = cache;
     }
-    numArray.add(val);
-    return numArray.toArray(new Integer[numArray.size()]);
-  }
+
+    @Override
+    public void run() {
+        int lastNum = cache.lastNum;
+        Integer[] lastFactors = cache.lastFactors;
+
+        if (request.content.equals(cache.lastNum)) {
+            System.out.println("Cache hit!");
+            Response response = new Response(lastNum, lastFactors);
+            System.out.println("The factors of " + response.num + " are: " + Arrays.toString(response.factors));
+            return;
+        }
+
+        Integer[] factors = this.factorsOf(request.content);
+        
+        if ((lastNum != 0) && !validateFactors(lastNum, lastFactors)) {
+            System.out.println("Incorrect factorization detected! Number: " + lastNum + " Computed Factors: " + Arrays.toString(lastFactors));
+            System.exit(1);
+        }
+        
+        cache.setCache(request.content, factors);
+        Response response = new Response(lastNum, lastFactors);
+        System.out.println("The factors of " + response.num + " are: " + Arrays.toString(response.factors));    
+    }
+
+    public Integer[] factorsOf(int val) {
+        List<Integer> numArray = new ArrayList<>();
+        int original = val;
+        
+        for (int i = 2; i <= Math.sqrt(val); i++) {
+            while (val % i == 0) {
+                numArray.add(i);
+                val /= i;
+            }
+        }
+        if (val > 1) {
+            numArray.add(val);
+        }
+        return numArray.toArray(new Integer[0]);
+    }
+
+    public boolean validateFactors(int number, Integer[] factors) {
+        int product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product == number;
+    }
 }
 
-
 class Request {
-  final Integer content;
+    final Integer content;
 
-  public Request(Integer content) {
-    this.content = content;
-  }
+    public Request(Integer content) {
+        this.content = content;
+    }
 }
 
 class Response {
-  final Integer[] content;
+  final Integer num;
+    final Integer[] factors;
 
-  public Response(Integer[] content) {
-    this.content = content;
-  }
+    public Response(Integer num, Integer[] factors) {
+        this.num = num;
+        this.factors = factors;
+    }
 }
 
 class Cache {
-  Integer lastNum;
-  Integer[] lastFactors;
+    volatile Integer lastNum;
+    volatile Integer[] lastFactors;
 
-  public Cache() {
-    lastNum = 0;
-    lastFactors = new Integer[0];
-  }
+    public Cache() {
+        lastNum = 0;
+        lastFactors = new Integer[0];
+    }
 
-  public void setCache(Integer num, Integer[] factors) {
-    lastNum = num;
-    lastFactors = factors;
-  }
+    public void setCache(Integer num, Integer[] factors) {
+        lastNum = num;
+        lastFactors = factors;
+    }
 
-  @Override
-  public String toString() {
-    return "Current Cache: \n" + "Last Number: " + lastNum + "\nLast Factors: " + Arrays.toString(lastFactors);
-  }
+    @Override
+    public String toString() {
+        return "Current Cache: \n" + "Last Number: " + lastNum + "\nLast Factors: " + Arrays.toString(lastFactors);
+    }
 }
