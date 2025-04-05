e196eefa033f465a399727de6a2cedf5f6605813
SponsoredByPuma <tillwanner01@gmail.com>
2025-04-05 21:45:25 +0200

    chore: remove unused import

--- app/src/main/java/concurrency/exercise3/Problem4.java
+++ app/src/main/java/concurrency/exercise3/Problem4.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Problem4 {
     public static void main(String[] args) {
--- app/src/main/java/concurrency/exercise3/Problem5.java
+++ app/src/main/java/concurrency/exercise3/Problem5.java
@@ -1,6 +1,5 @@
 package concurrency.exercise3;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
